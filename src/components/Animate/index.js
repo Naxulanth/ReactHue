75681bbb33facfa40a2b8822ae5c34de5617745d
Naxulanth <Naxulanth@gmail.com>
2019-01-01 09:51:58 +0300

    anim

--- src/components/Animate/index.js
+++ src/components/Animate/index.js
@@ -1,11 +1,9 @@
 import posed from 'react-pose'
+import { tween } from 'popmotion';
 
 const Animate = posed.div({
-    visible: { opacity: 1, height: 'initial' },
-    hidden: { opacity: 0, height: 0 },
-    transition: {
-        opacity: { ease: 'linear', duration: 500 },
-    }
+    visible: { opacity: 1, height: 'initial', transition: props => tween({ ...props, duration: 500 }) },
+    hidden: { opacity: 0, height: 0, transition: props => tween({ ...props, duration: 500 }) },
 });
 
 export default Animate;
