ab7baf3e419e992b437dd76a1293ad6054f594d5
Naxulanth <Naxulanth@gmail.com>
2019-01-01 10:19:47 +0300

    //

--- src/containers/RoomWidget/index.js
+++ src/containers/RoomWidget/index.js
@@ -6,14 +6,9 @@ import LightWidget from 'containers/LightWidget'
 
 import './index.css';
 import WidgetHeader from '../WidgetHeader';
-import { isEmpty } from 'utils';
 
 import { getXYtoRGB } from 'utils/colorConverter'
-
-import { getRoomById } from 'actions/rooms'
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
-
 
 class RoomWidget extends Component {
 
@@ -27,10 +22,15 @@ class RoomWidget extends Component {
     componentDidMount() {
         const { room, roomId } = this.props
         const lightIds = room[roomId].lights;
-        if (lightIds && !isEmpty(lightIds)) {
+        if (lightIds) {
             this.populateLights(lightIds);
         }
+    }
 
+    componentDidUpdate() {
+        const { lights, room, roomId } = this.props
+        const lightIds = room[roomId].lights;
+        this.main.style.borderColor = 'rgb(' + getXYtoRGB(lights[lightIds[0]].state.xy[0], lights[lightIds[0]].state.xy[1], lights[lightIds[0]].state.bri).join(',') + ')';
     }
 
     populateLights(arr) {
