781ad1d36b5de1586784d3a91a1338d2e9a21313
Naxulanth <Naxulanth@gmail.com>
2019-01-05 06:11:01 +0300

    scenes

--- src/containers/SceneWidget/index.js
+++ src/containers/SceneWidget/index.js
@@ -3,6 +3,7 @@ import { Row, Col } from 'reactstrap';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
 import { modifyLight } from 'actions/lights'
+import { getScenes, getScene, modifyScene } from 'actions/scenes'
 import './style.css';
 import { getXYtoRGB, getRGBtoXY } from 'utils/colorConverter'
 
@@ -17,18 +18,19 @@ class SceneWidget extends Component {
     }
 
     componentDidMount() {
-        
+        const { getScenes } = this.props;
+        getScenes();
     }
 
     render() {
         return (
             <div ref={(e) => this.main = e} className="scene-widget">
                 <Row>
-                    <Col lg="1"/>
-                    <Col lg="10"> 
-                    SceneWidget
+                    <Col lg="1" />
+                    <Col lg="10">
+                        SceneWidget
                     </Col>
-                    <Col lg="1"/>
+                    <Col lg="1" />
                 </Row>
             </div>
         )
@@ -38,12 +40,17 @@ class SceneWidget extends Component {
 
 const mapStateToProps = state => ({
     room: state.rooms.list,
-    lights: state.lights.list
+    lights: state.lights.list,
+    scenes: state.scenes.list,
+    activeScene: state.activeScenes
 })
 
 
 const mapDispatchToProps = dispatch => ({
-    modifyLight: bindActionCreators(modifyLight.request, dispatch)
+    modifyLight: bindActionCreators(modifyLight.request, dispatch),
+    getScenes: bindActionCreators(getScenes.request, dispatch),
+    getScene: bindActionCreators(getScene.request, dispatch),
+    modifyScene: bindActionCreators(modifyScene.request, dispatch),
 })
 
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux'
 
 import lights from './lights';
 import rooms from './rooms';
+import scenes from './scenes';
 
 const reducers = {
     lights,
-    rooms
+    rooms,
+    scenes
 }
 
 export default combineReducers(reducers)
