1e8c9c294b98d5fed81b1d15e4b937fe507ea2ea
Naxulanth <Naxulanth@gmail.com>
2019-01-12 16:43:36 +0300

    selector

--- src/containers/SceneWidget/index.js
+++ src/containers/SceneWidget/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux';
+import Select from 'react-select'
 import { modifyLight } from 'actions/lights'
 import { getScene, modifyScene } from 'actions/scenes'
 import './style.css';
@@ -14,7 +15,8 @@ class SceneWidget extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            roomScenes: {}
+            roomScenes: [],
+            selectedOption: null,
         }
         this.getRoomScenes = this.getRoomScenes.bind(this);
     }
@@ -24,35 +26,53 @@ class SceneWidget extends Component {
         if (prevProps.scenes !== scenes) {
             this.getRoomScenes();
         }
+        console.log(this.state.roomScenes)
     }
 
     getRoomScenes() {
         const { scenes, room, roomId } = this.props;
-        let roomScenes = {};
+        let roomScenes = [];
         Object.keys(scenes).forEach(scene => {
             if (JSON.stringify(scenes[scene].lights.sort()) === JSON.stringify(room[roomId].lights.sort())) {
-                roomScenes[scene] = scenes[scene];
+                roomScenes.push(scenes[scene]);
             }
         })
+        let selectors = [];
+        roomScenes.forEach(scene => {
+            let selector = { value: scene, label: scene.name }
+            selectors.push(selector);
+        })
         this.setState({
-            roomScenes: roomScenes
+            roomScenes: selectors
         })
     }
 
-    render() {
-        return (
-            <div ref={(e) => this.main = e} className="scene-widget">
-                <Row>
-                    <Col lg="1" />
-                    <Col lg="10">
-                        SceneWidget
-                    </Col>
-                    <Col lg="1" />
-                </Row>
-            </div>
-        )
+    handleChange(selectedOption) {
+        this.setState({ selectedOption });
     }
+    
 
+    render() {
+        const { selectedOption, roomScenes } = this.state;
+        if (roomScenes.length > 0) {
+            return (
+                <div ref={(e) => this.main = e} className="scene-widget">
+                    <Row>
+                        <Col lg="1" />
+                        <Col lg="10">
+                            <Select
+                                value={selectedOption}
+                                onChange={this.handleChange}
+                                options={roomScenes}
+                            />
+                        </Col>
+                        <Col lg="1" />
+                    </Row>
+                </div>
+            )
+        }
+        else return 'Loading...'
+    }
 }
 
 const mapStateToProps = state => ({
