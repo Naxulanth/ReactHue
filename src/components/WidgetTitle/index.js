aa2227b5cf93e9a7267ce6123ae26e0046f4a49a
Naxulanth <Naxulanth@gmail.com>
2019-01-31 18:22:53 +0300

    //

---
+++ src/components/WidgetTitle/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+
+import EditableLabel from 'react-inline-editing';
+
+const WidgetTitle = styled(EditableLabel)`
+font-size: 22px;
+height: 30px;
+line-height: 30px;
+display:inline-block;
+`;
+
+export default WidgetTitle;
