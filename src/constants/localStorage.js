b1c11b816751fd192fa5e44409a3216ce9b83b90
Naxulanth <Naxulanth@gmail.com>
2019-02-01 21:07:01 +0300

    //

--- src/constants/localStorage.js
+++ src/constants/localStorage.js
@@ -4,7 +4,6 @@ const parsed = queryString.parse(window.location.search);
 
 export let user = 'HUE_REACT_USERNAME';
 export let bridge = 'HUE_REACT_BRIDGE_IP';
-export let bridge_count 
 
 if (parsed['bridge']) {
     user += '_' + parsed['bridge'];
