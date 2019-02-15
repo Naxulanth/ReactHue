c92e1a0ec598ae9d6f3b2dd757e8bf69eb1bf59e
Naxulanth <Naxulanth@gmail.com>
2019-02-15 20:26:18 +0300

    timers

--- src/containers/Timers/index.js
+++ src/containers/Timers/index.js
@@ -2,17 +2,47 @@ import React, { Component } from 'react';
 import { Row, Col } from 'reactstrap';
 import { Button } from 'components/Button';
 import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux';
+import uuidv4 from 'uuid/v4';
 import './style.css';
 
 class Timers extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+        }
+        this.mapTimers = this.mapTimers.bind(this);
+    }
+
+
+    mapTimers(timers) {
+        return timers.map((timer) => {
+            console.log(timer)
+            return (
+                <Row key={uuidv4()}>
+                    <Col sm="8" md="8" lg="8" xl="8">
+                        {timer.name}
+                    </Col>
+                </Row>
+            )
+        })
+    }
+
     render() {
-        return (
-            <div className="timers routines">
-                Timers
-            </div>
-        );
+        const { mapTimers } = this;
+        const { schedules } = this.props;
+        if (schedules && schedules.timers) {
+            console.log('ok')
+            return (
+                <div className="timers routines">
+                    <div>
+                        Timers
+                    </div>
+                    {mapTimers(schedules.timers)}
+                </div>
+            );
+        }
+        else return null;
     }
 }
 
