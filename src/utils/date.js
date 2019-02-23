7cff018e93847b8a82ad5b200bce1f12b64079e4
Naxulanth <Naxulanth@gmail.com>
2019-02-23 05:43:48 +0300

    //

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,18 +2,22 @@ export function absolute(localtime, time) {
   let today = new Date();
   let offset = today.getTimezoneOffset() / 60;
   if (!localtime) {
-    localtime = new Date(time);
+    localtime = new Date(time.split("A")[0] + "Z");
     localtime.setUTCHours(localtime.getUTCHours() - offset);
   } else {
-    localtime = new Date(localtime);
+    console.log('h')
+    localtime = new Date(time.split("A")[0] + "Z");
   }
+  console.log(localtime)
   localtime.setDate(today.getDate());
   localtime.setMonth(today.getMonth());
   localtime.setFullYear(today.getFullYear());
   if (today > localtime) {
     localtime.setDate(today.getUTCDate() + 1);
   }
+  console.log(localtime)
   localtime.setHours(localtime.getHours() - offset);
+  console.log(localtime)
   localtime = localtime.toISOString().split(".")[0];
   console.log(localtime);
   return localtime;
