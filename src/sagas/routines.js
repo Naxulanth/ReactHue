93ae3846be78b12f3f5439fb51529230f7df043d
Naxulanth <Naxulanth@gmail.com>
2019-02-25 03:15:34 +0300

    //

--- src/sagas/routines.js
+++ src/sagas/routines.js
@@ -30,6 +30,10 @@ import * as schedulesApi from "../api/schedules";
 import * as resourcesActions from "../actions/resources";
 import * as resourcesApi from "../api/resources";
 
+shortid.characters(
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@"
+);
+
 export function* watchCreateRoutine() {
   yield takeLatest(ROUTINE_CREATE.REQUEST, createRoutine);
 }
@@ -39,11 +43,10 @@ export function* createRoutine({ body }) {
     let state = body.state;
     let props = body.props;
     let firstSchedule = {};
-    let resource = resourceObject(state.name, props.type);
-    shortid.characters(
-      "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$@"
-    );
     let shortId = shortid.generate();
+    firstSchedule.status = "disabled";
+    firstSchedule.recycle = true;
+    firstSchedule.autodelete = false;
     let lights = [];
     if (state.routineLights.length < 1) {
       Object.keys(props.roomList).forEach(roomKey => {
@@ -54,9 +57,6 @@ export function* createRoutine({ body }) {
     } else {
       lights = state.routineLights;
     }
-    firstSchedule.status = "disabled";
-    firstSchedule.recycle = true;
-    firstSchedule.autodelete = false;
     if (
       // recurring time
       Object.keys(state.days).some(function(day) {
@@ -72,20 +72,115 @@ export function* createRoutine({ body }) {
       firstSchedule.localtime = absolute(state.time, null, true);
     }
     if (props.type === "wake") {
+      // sensor
       const sensor = yield call(sensorsApi.createSensor, wakeSensor);
       yield put(sensorsActions.createSensor.success(sensor));
-      let sensorId = sensor.data[0].success.id;
-      resource.links.push("/sensors/" + sensorId);
+      const sensorId = sensor.data[0].success.id;
+
+      // first schedule
       firstSchedule.description = shortId + "_start wake up";
       firstSchedule.name = state.name;
       firstSchedule.command = sensorObject(sensorId);
-      const firstScheduleData = yield call(schedulesApi.createSchedule, firstSchedule);
+      const firstScheduleData = yield call(
+        schedulesApi.createSchedule,
+        firstSchedule
+      );
       yield put(schedulesActions.createSchedule.success(firstScheduleData));
-      let firstScheduleId = firstScheduleData.data[0].success.id;
-      resource.links.push("/schedules/" + firstScheduleId);
-      let secondSchedule = Object.assign({}, firstSchedule);
+      const firstScheduleId = firstScheduleData.data[0].success.id;
+      // first scene
+      const firstScene = yield call(
+        scenesApi.createScene,
+        sceneObject(false, type, lights)
+      );
+      yield put(scenesActions.createScene.success(firstScene));
+      const modifyFirstScene = yield call(
+        scenesActions.modifyScene,
+        firstScene,
+        createLightstates(lights, fadeSelect, type, false)
+      );
+      yield put(scenesActions.modifyScene.success(modifyFirstScene));
+      const firstSceneId = firstScene.data[0].success.id;
+      // second schedule
+      const secondSchedule = Object.assign({}, firstSchedule);
       secondSchedule.description = shortId + "_trigger end scene";
       secondSchedule.name = shortId;
+      secondSchedule.command = groupObject(firstSceneId);
+      const secondScheduleData = yield call(
+        schedulesApi.createSchedule,
+        secondSchedule
+      );
+      yield put(schedulesActions.createSchedule.success(secondScheduleData));
+      const secondScheduleId = secondScheduleData.data[0].success.id;
+      const secondScene = yield call(
+        scenesApi.createScene,
+        sceneObject(true, type, lights)
+      );
+      yield put(scenesActions.createScene.success(secondScene));
+      // second scene
+      const secondScene = secondScene.data[0].success.id;
+      const modifySecondScene = yield call(
+        scenesActions.modifyScene,
+        secondScene,
+        createLightstates(lights, fadeSelect, type, true)
+      );
+      yield put(scenesActions.modifyScene.success(modifySecondScene));
+      // main rule
+      const rule = yield call(
+        rulesApi.createRule,
+        ruleObject(
+          name,
+          createdSensor,
+          createdScene,
+          rooms,
+          createdSchedule,
+          true,
+          timeOff,
+          type
+        )
+      );
+      yield put(rulesActions.createRule.success(rule));
+      const ruleId = rule.data[0].success.id;
+      // timeoff rule & group
+      if (timeOff) {
+        createRoom(roomObject(lights));
+        createRule(
+          ruleObject(
+            name,
+            createdSensor,
+            createdScene,
+            createdRoom,
+            createdSchedule,
+            false,
+            timeOff,
+            type
+          )
+        );
+      }
+      // resources
+      let resource = resourceObject(state.name, props.type);
+      resource.links.push("/sensors/" + sensorId);
+      resource.links.push("/schedules/" + firstScheduleId);
+      resource.links.push("/schedules/" + secondScheduleId);
+      resource.links.push("/rules/" + ruleId);
+      resource.links.push("/scenes/" + firstSceneId);
+      resource.links.push("/scenes/" + secondSceneId);
+      if (timeOff) {
+        resource.links.push("/rules/" + timeoffRule);
+        resource.links.push("/groups/" + createdRoom);
+      }
+      if (rooms.length > 0) {
+        rooms.forEach(room => {
+          resource.links.push("/groups/" + room);
+        });
+      } else resource.links.push("/groups/" + 0);
+      const resourceData = yield call(resourcesApi.createResource, resource);
+      yield put(resourcesActions.createResource, resourceData);
+    } else if (type === "sleep") {
+    } else if (type === "routines") {
+      // clone scenes in roomScenes and pass
+      // 1 scene for each group, group 0 for home
+      // turn rooms off at @ rules routine end
+    } else if (type === "timers") {
     }
   } catch (e) {}
 }
